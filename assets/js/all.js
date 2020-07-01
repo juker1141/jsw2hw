0181c5e244ea3b2abcc9a6951dcf593f7dde59bf
juker1141 <50107434+juker1141@users.noreply.github.com>
2020-07-02 01:09:08 +0800

    Update 2020-07-01T17:09:06.645Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -19,7 +19,7 @@ var obj = {
     var products = this.data.products;
     var str = "";
     products.forEach(function (item) {
-      str += "\n      <div class=\"card rounded\">\n        <div class=\"card_img card-img-top rounded-top\"\n              style=\"background: url(".concat(item.imageUrl, ");\">\n        </div>\n        <div class=\"card-body pt-3 pb-2\">\n          <h5 class=\"card-title m-0\">").concat(item.title, "</h5>\n          <p class=\"card-text text-right mb-2\">\n            <s class=\"text-gray-2 fz_14\">").concat(item.origin_price, "</s><br>\n            ").concat(item.price, "\n          </p>\n          <div class=\"btn btn-success w-100\">Add to cart</div>\n        </div>\n      </div>");
+      str += "\n      <div class=\"card rounded mb-5\">\n        <div class=\"card_img card-img-top rounded-top\"\n              style=\"background: url(".concat(item.imageUrl, ");\">\n        </div>\n        <div class=\"card-body pt-3 pb-2\">\n          <h5 class=\"card-title m-0\">").concat(item.title, "</h5>\n          <p class=\"card-text text-right mb-2\">\n            <s class=\"text-gray-2 fz_14\">$").concat(item.origin_price, "</s><br>\n            $").concat(item.price, "\n          </p>\n          <div class=\"btn btn-success w-100\">Add to cart</div>\n        </div>\n      </div>");
     });
     productsList.innerHTML = str;
   }
